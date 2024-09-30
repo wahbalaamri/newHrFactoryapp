af5929d2813c3d00e02660a3a2673154f6dcf013
WahbAlaamri <wahb.alaamri@gmail.com>
2024-09-30 09:06:33 +0400

    Update UserSubscriptionsService.php

--- app/Http/services/UserSubscriptionsService.php
+++ app/Http/services/UserSubscriptionsService.php
@@ -28,7 +28,7 @@ class UserSubscriptionsService
         $plan_id = $subscription->plan_id;
         //get plan
         $plan = Plans::find($plan_id);
-        if ($plan->plan_type == 5  && $plan->surveys->where('is_active', true)->where('client_id', $user_id)->count() > 0) {
+        if ($plan->plan_type == 5  && $plan->surveys->where('survey_stat', true)->where('client_id', $user_id)->count() > 0) {
             return false;
         }
         if ($plan->plan_type == 5 && $plan->surveys->where('client_id', $user_id)->count() >= 3)
@@ -39,7 +39,7 @@ class UserSubscriptionsService
             return false;
         }
         //if plan is premium and it has one survey is active
-        if ($plan->plan_type == 1  && $plan->surveys->where('is_active', true)->where('client_id', $user_id)->count() > 0) {
+        if ($plan->plan_type == 1  && $plan->surveys->where('survey_stat', true)->where('client_id', $user_id)->count() > 0) {
             return false;
         }
         return true;
